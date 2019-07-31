927adcc29e8d33442daf54709b27871ccc84e94c
Dimo Chavdarov <d_chavdarov@hotmail.com>
2019-07-31 14:39:51 +0300

    Removed resources

---
+++ ExcelManagementQualysReports/Models/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelManagementQualysReports.Models
+{
+    public class Server
+    {
+        public string IP { get; set; }
+        public string SystemName { get; set; }
+
+        public string OSVersion { get; set; }
+
+        public string SystemStatus { get; set; }
+
+        public string SystemType { get; set; }
+
+        public string DowntimeContact { get; set; }
+
+        public string TechnicalOwner { get; set; }
+
+       
+        public string IpType { get; set; }
+
+        public string ConsoleIP { get; set; }
+
+
+    }
+}
