58a5119a0cceb5574d7a30a64345cbbff0a6d2d0
Dimo Chavdarov <d_chavdarov@hotmail.com>
2019-08-19 18:02:35 +0300

    Modified PathConstants

--- ExcelManagementQualysReports/Constants/PathConstants.cs
+++ ExcelManagementQualysReports/Constants/PathConstants.cs
@@ -7,19 +7,11 @@ namespace ExcelManagementQualysReports.Constants
 {
     public static class PathConstants
     {
-        public const string eslPath = "";
+        
 
         public const string directoryPath = @"C:\Users\dstoyanov5\OneDrive - DXC Production\source\repos\ExcelManagementQualysReports\ExcelManagementQualysReports\Resources\";
 
-        public const string MDC_ConfigPath = @"";
-
-        public const string MPC_ConfigPath = @"C:\Users\dstoyanov5\OneDrive - DXC Production\source\repos\ExcelManagementQualysReports\ExcelManagementQualysReports\Resources\MPC_ConfigFindingsJuly2019.xlsx";
-
-        public const string MPC_MissingPatchPath = @"C:\Users\dstoyanov5\OneDrive - DXC Production\source\repos\ExcelManagementQualysReports\ExcelManagementQualysReports\Resources\MPC_MissingPatchFindingsJuly2019.xlsx";
-
-        public const string MPC_EOLfindings = @"C:\Users\dstoyanov5\OneDrive - DXC Production\source\repos\ExcelManagementQualysReports\ExcelManagementQualysReports\Resources\MPC_EOLfindingsJuly2019.xlsx";
-
-        public const string MDC_MissingPatchVulnerabilities = @"C:\Users\dstoyanov5\OneDrive - DXC Production\source\repos\ExcelManagementQualysReports\ExcelManagementQualysReports\Resources\MDC_PatchableConfirmedVulnerabilities.xlsx";
+       
 
     }
 }
